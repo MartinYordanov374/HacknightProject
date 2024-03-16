dc5f588f3d53b5a7214a7c52d3b4d5d8359ea704
MartinYordanov374 <martinyordanov284@gmail.com>
2024-03-16 10:40:34 +0200

    Add return home link to the error page

--- summarizer/src/components/ErrorPage.js
+++ summarizer/src/components/ErrorPage.js
@@ -9,6 +9,7 @@ export default function ErrorPage() {
             <div className="text-center">
                 <h1>404</h1>
                 <p>Oops! Page not found.</p>
+                <a href='/'>Return home.</a>
             </div>
         </div>
       </div>
