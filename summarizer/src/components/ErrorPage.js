787cd0e7b11c37b859281d8e858c119ce194fdcd
MartinYordanov374 <martinyordanov284@gmail.com>
2024-03-16 10:38:51 +0200

    Add error page styling andcustom message

--- summarizer/src/components/ErrorPage.js
+++ summarizer/src/components/ErrorPage.js
@@ -1,7 +1,18 @@
-import React from 'react'
-
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import '../Styles/ErrorPage.css'
 export default function ErrorPage() {
   return (
-    <div>ErrorPage</div>
-  )
+    <div className="errorMessageWrapper">
+      <div className='row'>
+        <div className='col'>
+            <div className="text-center">
+                <h1>404</h1>
+                <p>Oops! Page not found.</p>
+            </div>
+        </div>
+      </div>
+          
+    </div>
+  );
 }
