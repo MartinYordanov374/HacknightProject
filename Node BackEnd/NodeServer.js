119ac55b2f6e39b1b12f1db9300c4925d8716e37
MartinYordanov374 <martinyordanov284@gmail.com>
2024-03-16 11:40:52 +0200

    Set up the basic endpoint of express server

--- Node BackEnd/NodeServer.js
+++ Node BackEnd/NodeServer.js
@@ -4,6 +4,15 @@ const express = require('express')
 const app = express();
 const port = 3000
 
+
+app.get('/getSummary/:summaryId', (req,res) => {
+    console.log('getting summary')
+})
+
+app.get('/summarizeFile/', (req,res) => {
+    console.log('summarizing file')
+})
+
 app.listen(port, () => {
     console.log('server listening on port 3000')
 })
